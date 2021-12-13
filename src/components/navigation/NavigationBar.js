3c5afc0cf7c0c49e3c0ca7c6edcd3b55b61e1405
Mel Incognito <melxincognito@gmail.com>
2021-12-12 22:34:14 -0700

    integrated links with mui tab components

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Tabs,
+  Tab,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 
 export default function NavigationBar() {
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ bgcolor: "pink" }}>
       <Toolbar>
         <Typography variant="h5">Nav Bar</Typography>
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/about">About Me</Link>
-          <Link to="/projects">My Projects</Link>
-        </div>
+        <Container>
+          <Tabs>
+            <Tab label="Home" component={Link} to="/" />
+            <Tab label="About Me" component={Link} to="/about" />
+            <Tab label="My Projects" component={Link} to="/projects" />
+          </Tabs>
+        </Container>
       </Toolbar>
     </AppBar>
   );
