abd8fe8a08bebb42a6b485dda20fbdaaf543e5f0
Mel Incognito <melxincognito@gmail.com>
2021-12-31 14:35:02 -0700

    added experience accordions

--- src/pages/MyProjects.js
+++ src/pages/MyProjects.js
@@ -1,4 +1,14 @@
-import { Container, Typography, Card, CardContent, Link } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Card,
+  CardContent,
+  Link,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import React from "react";
 
 export default function MyProjects() {
@@ -20,43 +30,11 @@ export default function MyProjects() {
               from scratch with React to really learn all the nuts and bolts of
               programming! I plan to continue building and maintaining sites so
               I can learn to develop reliable web applications. Once I become
-              more advanced with ReactJS I plan to learn to develop reliable
-              mobile applications with React Native.
+              more advanced with ReactJS I plan to develop reliable mobile
+              applications with React Native.
             </Typography>
           </CardContent>
 
-          <CardContent>
-            <Typography>
-              Here's a small list of the websites I've created or maintained:
-              <ul>
-                {" "}
-                <li>
-                  <Link
-                    href="https://www.adventuringavocado.com"
-                    target="_blank"
-                  >
-                    www.adventuringavocado.com
-                  </Link>
-                </li>
-                <li>
-                  {" "}
-                  <Link href="http://tmg-construction.com/" target="_blank">
-                    www.tmg-construction.com
-                  </Link>
-                </li>
-              </ul>
-            </Typography>
-          </CardContent>
-          <CardContent>
-            {" "}
-            <Typography variant="body1">
-              {" "}
-              I know the list is small but I know my passion will allow me to
-              progress forward quickly. My goal by the end of 2022 is to develop
-              at least three large scale applications in my portfolio using
-              React!
-            </Typography>
-          </CardContent>
           <CardContent>
             <Typography variant="body1">
               Feel free to check out my{" "}
@@ -65,6 +43,120 @@ export default function MyProjects() {
               </Link>{" "}
               if you're interested to see code from my past projects!
             </Typography>
+          </CardContent>
+
+          <CardContent>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Web Development Projects</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Here's a small list of the websites I've created or
+                  maintained:
+                  <ul>
+                    {" "}
+                    <li>
+                      <Link
+                        href="https://www.adventuringavocado.com"
+                        target="_blank"
+                      >
+                        www.adventuringavocado.com
+                      </Link>
+                    </li>
+                    <li>
+                      {" "}
+                      <Link href="http://tmg-construction.com/" target="_blank">
+                        www.tmg-construction.com
+                      </Link>
+                    </li>
+                  </ul>
+                </Typography>
+                <Typography variant="body1">
+                  {" "}
+                  I know the list is small but I know my passion will allow me
+                  to progress forward quickly. My goal by the end of 2022 is to
+                  develop at least three large scale applications in my
+                  portfolio using React!
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+              >
+                <Typography>Professional Experience</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  <ul>
+                    <li>
+                      {" "}
+                      Accounting Specialist - Maricopa County - Phoenix, USA
+                    </li>
+                    <li>
+                      {" "}
+                      Project Manager - TMG Construction LLC - Phoenix, USA
+                    </li>
+                    <li>
+                      {" "}
+                      Customer Success Manager - RetraceIt Technologies -
+                      Madrid, ES{" "}
+                    </li>
+                  </ul>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+              >
+                <Typography>Other Experience</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  <ul>
+                    <li>
+                      {" "}
+                      English Teaching Assistant - Ministerio de Educación,
+                      Cultura y Deporte - Madrid, ES
+                    </li>
+                    <li> Private English Teacher - Freelance - Madrid, ES </li>
+                    <li> Bookkeeper - TMG Construction LLC - REMOTE </li>
+                    <li> Waitress - Various </li>
+                  </ul>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+              >
+                <Typography>Hobbies</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  <ul>
+                    <li> Cooking </li>
+                    <li> Tattooing oranges </li>
+                    <li> Digital Art </li>
+                    <li> Solo Travel </li>
+                    <li> Programming </li>
+                    <li> Learning new languages </li>
+                  </ul>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
           </CardContent>
         </CardContent>
       </Card>
