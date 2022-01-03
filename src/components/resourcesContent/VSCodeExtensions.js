5229fab69a548f723fcfbf25d0ce6165982203e5
Mel Incognito <melxincognito@gmail.com>
2022-01-02 18:42:22 -0700

    finished content por fin :3

--- src/components/resourcesContent/VSCodeExtensions.js
+++ src/components/resourcesContent/VSCodeExtensions.js
@@ -6,6 +6,7 @@ import {
   Accordion,
   AccordionSummary,
   AccordionDetails,
+  Link,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
@@ -33,9 +34,66 @@ export default function VSCodeExtensions() {
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
+              There are a lot of different productivity extensions out there,
+              these are just the ones I use on a regular basis.
+              <ul>
+                <li>
+                  <Link
+                    href="https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode"
+                    target="_blank"
+                  >
+                    Prettier Code Formatter
+                  </Link>{" "}
+                  is amazing, I don't know how I went so long without this
+                  extension. It's an opinionated code formatter that formats
+                  your code so it's easily readable. When you first download it
+                  you have to do a special key combo to format it, change the
+                  settings so it formats on save and it's a lot more useful. If
+                  your code has an error like a missing bracket it won't format
+                  on save so it's a good cue to take another look at your code
+                  before you run.
+                </li>
+                <li>
+                  {" "}
+                  <Link
+                    href="https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer"
+                    target="_blank"
+                  >
+                    Live Server
+                  </Link>{" "}
+                  is another good extension. It launches a development server
+                  with the page you're creating and it reloads live with each
+                  save so you don't have to keep relaunching every time you make
+                  a change.
+                </li>
+                <li>
+                  <Link
+                    href="https://marketplace.visualstudio.com/items?itemName=dsznajder.es7-react-js-snippets"
+                    target="_blank"
+                  >
+                    ES7 React/Redux/GraphQL/React-Native snippits
+                  </Link>{" "}
+                  is a total time saver. It comes with snippits so you dont have
+                  to type out commonly used commands like import React from
+                  'react', it can make a skeleton of a functional/class
+                  component, etc. The things that aren't difficult but can be
+                  tedious.
+                </li>
+                <li>
+                  <Link
+                    href="https://marketplace.visualstudio.com/items?itemName=2gua.rainbow-brackets"
+                    target="_blank"
+                  >
+                    Rainbow Brackets
+                  </Link>{" "}
+                  color codes your round, squiggly and square brackets so it
+                  stands out from the rest of your code and you can locate which
+                  bracket goes with which. It saves you a lot of trouble, I
+                  don't know how many times I spent hours looking for the reason
+                  my project was crashing only to find I was missing a single }
+                  🙃
+                </li>
+              </ul>
             </Typography>
           </AccordionDetails>
         </Accordion>
@@ -45,13 +103,42 @@ export default function VSCodeExtensions() {
             aria-controls="panel2a-content"
             id="panel2"
           >
-            <Typography>Theme Extensions</Typography>
+            <Typography> Theme Extensions</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
+              Once I found out you can change the color theme of your code
+              editor I wondered how I was looking at the default one for so
+              long! You're going to be looking at your editor for hours on end
+              so it may as well be appealing to your eyes. There's plenty out
+              there so you can look around to see which ones you like. I like
+              high contrast themes, so far my favorites have been:
+              <ul>
+                <li>
+                  <Link
+                    href="https://marketplace.visualstudio.com/items?itemName=dracula-theme.theme-dracula"
+                    target="_blank"
+                  >
+                    Dracula Official
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://marketplace.visualstudio.com/items?itemName=ahmadawais.shades-of-purple"
+                    target="_blank"
+                  >
+                    Shades of Purple
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://marketplace.visualstudio.com/items?itemName=GitHub.github-vscode-theme"
+                    target="_blank"
+                  >
+                    GitHub Theme - GitHub Dark High Contrast
+                  </Link>
+                </li>
+              </ul>
             </Typography>
           </AccordionDetails>
         </Accordion>
