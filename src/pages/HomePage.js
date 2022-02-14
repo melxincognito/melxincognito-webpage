857e110a0b9927e8b788388c19cda404cfb97b99
Mel Incognito <melxincognito@gmail.com>
2022-02-14 11:42:16 -0700

    edited content

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -30,21 +30,21 @@ export default function HomePage() {
       </CardContent>
       <CardContent>
         <Typography variant="body1">
-          Welcome to my page! I'm Mel, a Software Developer who makes Web
+          Welcome to my page! I'm a Software Developer who makes Web
           applications using React.JS and Python. I learned how to program
           applications a few years after I graduated from Arizona State
-          University with a Bachelors in Business. I utilize my background in
-          both business and programming to work with my clients to ensure their
-          applications are both high quality and good from a marketing
+          University with a Bachelors in Business Law. I utilize my background
+          in both business and programming to work with my clients to ensure
+          their applications are both high quality and good from a marketing
           standpoint.
         </Typography>
       </CardContent>
       <CardContent>
         <Typography variant="body1">
           {" "}
-          Feel free to take a look around! You can click on the top tabs to read
-          a little about me, see the services I am currently able to offer and
-          send me a message using my contact form.
+          Feel free to take a look around to find out more! You can click on the
+          top tabs to read a little about me, see the services I am able to
+          offer and request a quote using my contact form.
         </Typography>
       </CardContent>
     </MainCard>
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,5 +1,12 @@
-import { CardContent, Typography } from "@mui/material";
 import React from "react";
+import {
+  CardContent,
+  Typography,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MainCard from "../components/ui/MainCard";
 export default function Services() {
   const headerStyles = {
@@ -7,8 +14,17 @@ export default function Services() {
     alignContent: "center",
     justifyContent: "center",
     color: "#330e62",
+    boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.4)",
+    margin: 2,
+    borderRadius: 4,
   };
 
+  const accordionColorTwo = {
+    backgroundColor: "rgba(231, 183, 241, 0.13)",
+  };
+  const accordionColorOne = {
+    backgroundColor: "rgba(155, 35, 179, 0.10)",
+  };
   return (
     <>
       {" "}
@@ -17,35 +33,92 @@ export default function Services() {
           <Typography variant="h4"> Services</Typography>
         </CardContent>
         <CardContent>
-          Website Design & Development, Website maintenance, custom QR codes,
-          5-10 year custom domain name registration, SEO optimization
-        </CardContent>
-
-        <CardContent>
-          Web Design and Development. I work with my clients from start to
-          finish. We'll start with choosing a design for your site to fit your
-          business' particular needs. After we've settled on a design I'll work
-          on creating everything using the best development tools for your
-          project.
-        </CardContent>
-        <CardContent>
-          SEO Optimization. Increase your business visibility with SEO
-          optimization.{" "}
-        </CardContent>
-        <CardContent>
-          Website Maintenance. The web is always changing with new practices
-          being introduced everyday. I'll ensure everything on your website
-          stays up to date and is working smoothly.
-        </CardContent>
-        <CardContent>
-          Domain name registration. Your website wouldn't be complete without a
-          custom domain name. I'll do all the work have your site hosted online
-          at a domain that's associated with your business.
-        </CardContent>
-        <CardContent>
-          QR Code creation. Gone are the days where you manually input a website
-          name. In todays world your website wouldn't be complete without a QR
-          code that'll allow users to go directly to your site.
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1"
+              id="panel1"
+              sx={accordionColorOne}
+            >
+              <Typography>Website Design & Development </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                I work with my clients from start to finish. We'll start with
+                choosing a design for your site to fit your business' particular
+                needs. After we've settled on a design I'll work on creating
+                everything using the best development tools for your project.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2"
+              id="panel2"
+              sx={accordionColorTwo}
+            >
+              <Typography>Website Maintenance</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                The web is always changing with new practices being introduced
+                everyday. Whether it's updating technological practices or
+                updating content, I'll ensure everything from on your website
+                stays up to date and is working smoothly.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3"
+              id="panel3"
+              sx={accordionColorOne}
+            >
+              <Typography>Domain name registration</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Your website wouldn't be complete without a custom domain name.
+                I'll do all the work have your site hosted online at a domain
+                that's associated with your business.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4"
+              id="panel4"
+              sx={accordionColorTwo}
+            >
+              <Typography>Custom QR Code</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Gone are the days where you manually input a website name. In
+                todays world your website wouldn't be complete without a QR code
+                that'll allow users to go directly to your site.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel5"
+              id="panel5"
+              sx={accordionColorOne}
+            >
+              <Typography>SEO Optimization</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Get the most out of your website and increase your business
+                visibility with SEO optimization
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
         </CardContent>
       </MainCard>
     </>
