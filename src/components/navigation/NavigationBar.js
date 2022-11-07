390c29e825c843ced8699e0496dd3f48652cc6f4
Mel Incognito <melxincognito@gmail.com>
2022-11-06 20:45:44 -0700

    removed default aria label for mobile menu icon button

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -171,7 +171,6 @@ const NavigationBar = () => {
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
-              aria-label="account of current user"
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
